c101b65cd00f15804141901a749a477413b2b62c
Cedric-M <cirdec95@gmail.com>
2020-03-04 16:26:54 +0100

    Update: naming convention

--- paramiko_example_python3.py
+++ paramiko_example_python3.py
@@ -1,15 +1,16 @@
 import paramiko
-k = paramiko.RSAKey.from_private_key_file("/path-to-your-key/privatekey.pem")
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+private_key = paramiko.RSAKey.from_private_key_file("/path-to-your-key/privatekey.pem")
+sshclient = paramiko.SSHClient()
+sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print ("DEBUG: Connecting")
-c.connect( hostname = "<add-url-or-ip-address-here>", username = "<add-username-here>", pkey = k )
+sshclient.connect( hostname = "<add-url-or-ip-address-here>", username = "<add-username-here>", pkey = private_key )
 print ("DEBUG: Connected")
 commands = [ "/home/ubuntu/firstscript.sh", "/home/ubuntu/secondscript.sh" ]
 for command in commands:
 	print ("Executing {}".format( command ))
-	stdin , stdout, stderr = c.exec_command(command)
+	stdin , stdout, stderr = sshclient.exec_command(command)
 	print (stdout.read().decode('ascii'))
 	print("Errors")
 	print (stderr.read())
-c.close()
+sshclient.close()
