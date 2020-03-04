5c6cbcf8cf8b2d8130ebbe69603c8e45c089a1fe
Cedric-M <cirdec95@gmail.com>
2020-03-04 14:45:23 +0100

    Add: paramiko_example_python3.py

---
+++ paramiko_example_python3.py
@@ -0,0 +1,15 @@
+import paramiko
+k = paramiko.RSAKey.from_private_key_file("/path-to-your-key/privatekey.pem")
+c = paramiko.SSHClient()
+c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print ("DEBUG: Connecting")
+c.connect( hostname = "<add-url-or-ip-address-here>", username = "<add-username-here>", pkey = k )
+print ("DEBUG: Connected")
+commands = [ "/home/ubuntu/firstscript.sh", "/home/ubuntu/secondscript.sh" ]
+for command in commands:
+	print ("Executing {}".format( command ))
+	stdin , stdout, stderr = c.exec_command(command)
+	print (stdout.read())
+	print("Errors")
+	print (stderr.read())
+c.close()
