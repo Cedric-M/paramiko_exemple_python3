385bf72a9f67ae0e0c9507dbc7f86ddb3236e448
Cedric-M <cirdec95@gmail.com>
2020-03-05 17:21:11 +0100

    Update: format function + add variables

--- paramiko_example_python3.py
+++ paramiko_example_python3.py
@@ -1,14 +1,18 @@
 import paramiko
 
+#Variables:
+ssh_hostname='<add-url-or-ip-address-here>'
+ssh_username='<add-username-here>'
+
 private_key = paramiko.RSAKey.from_private_key_file("/path-to-your-key/privatekey.pem")
 sshclient = paramiko.SSHClient()
 sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print ("DEBUG: Connecting")
-sshclient.connect( hostname = "<add-url-or-ip-address-here>", username = "<add-username-here>", pkey = private_key )
+sshclient.connect(hostname = ssh_hostname, username = ssh_username, pkey = private_key)
 print ("DEBUG: Connected")
 commands = [ "/home/ubuntu/firstscript.sh", "/home/ubuntu/secondscript.sh" ]
 for command in commands:
-	print ("Executing {}".format( command ))
+	print (f"Executing {command}")
 	stdin , stdout, stderr = sshclient.exec_command(command)
 	print (stdout.read().decode('ascii'))
 	print("Errors")
